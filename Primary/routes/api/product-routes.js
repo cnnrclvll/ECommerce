373aba4131f05df6a512d942a2984d7168554b7f
Connor Colvill <cnnrclvll@gmail.com>
2024-06-10 06:40:56 -0400

    product routes working

--- Primary/routes/api/product-routes.js
+++ Primary/routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try { // DONE: get route for all products
+    const prodData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!prodData) {
+      res.status(404).json({ message: 'No products in record!' });
+      return;
+    }
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try { // DONE: get route for a single product
+    const prodData = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!prodData) {
+      res.status(404).json({ message: 'No matching product in record!' });
+      return;
+    }
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
